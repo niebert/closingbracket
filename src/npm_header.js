e82f4e4d34b0f0edb526948634566b52c890e75a
Engelbert Niehaus <niehaus@uni-landau.de>
2020-10-16 19:05:23 +0200

    Added feature to package  - filesaver.js added to support downloading the generated expanded latex file

--- src/npm_header.js
+++ src/npm_header.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: BracketHandler
  Package:  closingbracket
- Version:  1.0.2  Date: 2020/10/16 18:19:48
+ Version:  1.0.4  Date: 2020/10/16 19:04:09
  Homepage: https://github.com/niebert/closingbracket#readme
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2020/10/16 18:19:48
+ Date:     2020/10/16 19:04:09
  Require Module with:
     const BracketHandler = require('closingbracket');
  JSHint: installation with 'npm install jshint -g'
